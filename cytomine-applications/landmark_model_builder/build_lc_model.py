f191a27a6d62605961f2cbe67601ebc5d9d56910
Rémy Vandaele <remy.vandaele@gmail.com>
2017-04-18 17:21:18 +0200

    download.py

--- cytomine-applications/landmark_model_builder/build_lc_model.py
+++ cytomine-applications/landmark_model_builder/build_lc_model.py
@@ -1,20 +1,18 @@
 from sklearn.externals import joblib
-from procrustes import *
 from download import *
 import optparse
 import cytomine
 from VotingTreeRegressor import *
 from build_integral_image import *
+from ldmtools import *
 
-"""
-PHASE 1: VOTE MAPS
-"""
 
 def pad_integral(intg):
-	(h,w) = intg.shape
-	nintg = np.zeros((h+1,w+1))
-	nintg[1:,1:]=intg
+	(h, w) = intg.shape
+	nintg = np.zeros((h+1, w+1))
+	nintg[1:, 1:] = intg
 	return nintg
+
 
 def generate_2_horizontal(w, n):
 	coords = np.zeros((n, 4))
@@ -65,7 +63,6 @@ def generate_2d_coordinates_horizontal(coords):
 	(n, quatre) = coords.shape
 	x = np.zeros((n, 6))
 	y = np.zeros((n, 6))
-
 	w = np.floor(0.5 * ((coords[:, 2] - coords[:, 0]) + 1.)).astype('int')
 	x[:, 0] = coords[:, 0] - 1
 	y[:, 0] = coords[:, 1] - 1
@@ -79,7 +76,6 @@ def generate_2d_coordinates_horizontal(coords):
 	y[:, 4] = y[:, 3]
 	x[:, 5] = x[:, 2]
 	y[:, 5] = y[:, 4]
-
 	return x.astype('int'), y.astype('int')
 
 
@@ -87,7 +83,6 @@ def generate_2d_coordinates_vertical(coords):
 	(n, quatre) = coords.shape
 	x = np.zeros((n, 6))
 	y = np.zeros((n, 6))
-
 	w = np.floor(0.5 * ((coords[:, 3] - coords[:, 1]) + 1)).astype('int')
 	x[:, 0] = coords[:, 0] - 1
 	y[:, 0] = coords[:, 1] - 1
@@ -110,7 +105,6 @@ def generate_3d_coordinates_horizontal(coords):
 	x = np.zeros((n, 8))
 	y = np.zeros((n, 8))
 	w = np.floor(((coords[:, 2] - coords[:, 0]) + 1.) / 3.).astype('int')
-
 	x[:, 0] = coords[:, 0] - 1
 	y[:, 0] = coords[:, 1] - 1
 	x[:, 1] = x[:, 0] + w
@@ -127,7 +121,6 @@ def generate_3d_coordinates_horizontal(coords):
 	y[:, 6] = y[:, 4]
 	x[:, 7] = x[:, 3]
 	y[:, 7] = y[:, 4]
-
 	return x.astype('int'), y.astype('int')
 
 
@@ -136,7 +129,6 @@ def generate_3d_coordinates_vertical(coords):
 	x = np.zeros((n, 8))
 	y = np.zeros((n, 8))
 	w = np.floor(((coords[:, 3] - coords[:, 1]) + 1.) / 3.).astype('int')
-
 	x[:, 0] = coords[:, 0] - 1
 	y[:, 0] = coords[:, 1] - 1
 	x[:, 1] = coords[:, 2]
@@ -153,7 +145,6 @@ def generate_3d_coordinates_vertical(coords):
 	y[:, 6] = coords[:, 3]
 	x[:, 7] = x[:, 5]
 	y[:, 7] = y[:, 6]
-
 	return x.astype('int'), y.astype('int')
 
 
@@ -194,10 +185,11 @@ def generate_square_coordinates(coords):
 
 	return x.astype('int'), y.astype('int')
 
+
 def compute_features(intg, x, y, coords_h2, coords_v2, coords_h3, coords_v3, coords_sq):
 	pad_intg = pad_integral(intg)
-	x = x + 1
-	y = y + 1
+	x += 1
+	y += 1
 	(h, w) = pad_intg.shape
 	h -= 1
 	w -= 1
@@ -307,24 +299,22 @@ def compute_features(intg, x, y, coords_h2, coords_v2, coords_h3, coords_v3, coo
 
 	return dataset
 
+
 def build_dataset_image_offset(repository, image_number, xc, yc, dmax, nsamples, h2, v2, h3, v3, sq):
 	intg = build_integral_image(readimage(repository, image_number))
-	(h, w) = intg.shape
-
 	rep_x = np.random.randint(-dmax, dmax + 1, nsamples)
 	rep_y = np.random.randint(-dmax, dmax + 1, nsamples)
-
 	xs = xc + rep_x
 	ys = yc + rep_y
-
 	rep = np.zeros((nsamples, 2))
 	rep[:, 0] = rep_x
 	rep[:, 1] = rep_y
-
 	return compute_features(intg, xs, ys, h2, v2, h3, v3, sq), rep
+
 
 def bdio_helper(jobargs):
 	return build_dataset_image_offset(*jobargs)
+
 
 def build_dataset_image_offset_mp(repository, xc, yc, ims, dmax, nsamples, h2, v2, h3, v3, sq, n_jobs):
 	nimages = xc.size
@@ -354,8 +344,52 @@ def build_dataset_image_offset_mp(repository, xc, yc, ims, dmax, nsamples, h2, v
 
 	return DATASET, REP, IMG
 
-if __name__ == "__main__":
 
+def procrustes(x_coords, y_coords):
+	(ndata, nldms) = x_coords.shape
+
+	mean_x = np.mean(x_coords, axis=1)
+	mean_y = np.mean(y_coords, axis=1)
+	coords_centered = np.zeros((ndata, 2 * nldms))
+	for i in range(nldms):
+		coords_centered[:, i] = x_coords[:, i] - mean_x
+		coords_centered[:, i + nldms] = y_coords[:, i] - mean_y
+
+	coords_centered[0, :] = coords_centered[0, :] / np.linalg.norm(coords_centered[0, :])
+
+	c = np.zeros((2, nldms))
+	for i in range(1, ndata):
+		a = np.dot(coords_centered[i, :], coords_centered[0, :]) / (np.linalg.norm(coords_centered[i, :]) ** 2)
+		b = np.sum((coords_centered[i, 0:nldms] * coords_centered[0, nldms:]) - (coords_centered[0, :nldms] * coords_centered[i, nldms:])) / (np.linalg.norm(coords_centered[i, :]) ** 2)
+		s = np.sqrt((a ** 2) + (b ** 2))
+		theta = np.arctan(b / a)
+
+		scaling_matrix = s * np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+		c[0, :] = coords_centered[i, 0:nldms]
+		c[1, :] = coords_centered[i, nldms:]
+
+		new_c = np.dot(scaling_matrix, c)
+
+		coords_centered[i, 0:nldms] = new_c[0, :]
+		coords_centered[i, nldms:] = new_c[1, :]
+
+	return coords_centered
+
+
+def apply_pca(coords, k):
+	(ndata, nldms) = coords.shape
+	m = np.mean(coords, axis=0).reshape((nldms, 1))
+	mat = np.zeros((nldms, nldms))
+	for i in range(ndata):
+		v = coords[i, :].reshape((nldms, 1))
+		d = v - m
+		mat += np.dot(d, d.T)
+	mat /= float(nldms - 1)
+	(values, vectors) = np.linalg.eig(mat)
+	return m, vectors[:, 0:k]
+
+
+def main():
 	p = optparse.OptionParser(description='Cytomine Landmark Detection : Model building', prog='Cytomine Landmark Detector : Model builder', version='0.1')
 	p.add_option('--cytomine_host', type="string", default='beta.cytomine.be', dest="cytomine_host", help="The Cytomine host (eg: beta.cytomine.be, localhost:8080)")
 	p.add_option('--cytomine_public_key', type="string", default='XXX', dest="cytomine_public_key", help="Cytomine public key")
@@ -371,7 +405,7 @@ if __name__ == "__main__":
 	p.add_option('--model_D_MAX', type='int', default=6, dest='model_D_MAX', help="D_MAX parameter.")
 	p.add_option('--model_n_samples', type='int', default=200, dest='model_n_samples', help="Number of samples for phase 1.")
 	p.add_option('--model_W', type='int', default=100, dest='model_W', help="Window size for feature extraction.")
-	p.add_option('--model_n', type='int', default=20, dest='model_n',help="Number of samples extracted.")
+	p.add_option('--model_n', type='int', default=20, dest='model_n', help="Number of samples extracted.")
 	p.add_option('--model_T', type='int', default=50, dest='model_T', help="Number of trees for phase 1.")
 	p.add_option('--model_step', type='int', default=3, dest='model_step', help="Step for prediction for phase 1.")
 	p.add_option('--model_n_reduc', type='int', default=2, dest='model_n_reduc', help="Size for PCA reduction in phase 2.")
@@ -388,44 +422,23 @@ if __name__ == "__main__":
 	cytomine_connection = cytomine.Cytomine(options.cytomine_host, options.cytomine_public_key, options.cytomine_private_key, base_path=options.cytomine_base_path, working_path=options.cytomine_working_path, verbose=options.verbose)
 
 	current_user = cytomine_connection.get_current_user()
-	run_by_user_job = False
 	if not current_user.algo:
 		user_job = cytomine_connection.add_user_job(options.cytomine_id_software, options.cytomine_id_project)
 		cytomine_connection.set_credentials(str(user_job.publicKey), str(user_job.privateKey))
 	else:
 		user_job = current_user
 
-	run_by_user_job = True
-
 	job = cytomine_connection.get_job(user_job.job)
-	job = cytomine_connection.update_job_status(job, status=job.RUNNING, progress=0, status_comment="Bulding model...")
-	job_parameters = {}
-	job_parameters['cytomine_id_terms'] = options.cytomine_id_terms
-	job_parameters['model_njobs'] = options.model_njobs
-	job_parameters['model_D_MAX'] = options.model_D_MAX
-	job_parameters['model_n_samples'] = options.model_n_samples
-	job_parameters['model_W'] = options.model_W
-	job_parameters['model_n'] = options.model_n
-	job_parameters['model_T'] = options.model_T
-	job_parameters['model_step'] = options.model_step
-	job_parameters['model_n_reduc'] = options.model_n_reduc
-	job_parameters['model_R_MAX'] = options.model_R_MAX
-	job_parameters['model_R_MIN'] = options.model_R_MIN
-	job_parameters['model_alpha'] = options.model_alpha
+	cytomine_connection.update_job_status(job, status=job.RUNNING, progress=0, status_comment="Bulding model...")
 	model_repo = options.model_save_to
 
 	if not os.path.isdir(model_repo):
 		os.mkdir(model_repo)
 
-	if not run_by_user_job:
-		job_parameters_values = cytomine_connection.add_job_parameters(user_job.job, cytomine_connection.get_software(options.cytomine_id_software), job_parameters)
-
 	download_images(cytomine_connection, options.cytomine_id_project)
 	download_annotations(cytomine_connection, options.cytomine_id_project, options.cytomine_working_path)
 
 	repository = options.cytomine_working_path + str(options.cytomine_id_project) + '/'
-	txt_repository = repository + 'txt/'
-
 	(xc, yc, xr, yr, ims, term_to_i, i_to_term) = getallcoords(repository.rstrip('/') + '/txt/')
 	(nims, nldms) = xc.shape
 
@@ -442,46 +455,41 @@ if __name__ == "__main__":
 		term_list = term_to_i.keys()
 		Xc = xc
 		Yc = yc
-	(nims,nldms) = Xc.shape
-	im_list = None
+	(nims, nldms) = Xc.shape
+
 	if options.cytomine_training_images != 'all':
 		im_list = [int(p) for p in options.cytomine_training_images.split(',')]
 	else:
 		im_list = ims
 
-	X = np.zeros((len(im_list),nldms))
-	Y = np.zeros((len(im_list),nldms))
+	X = np.zeros((len(im_list), nldms))
+	Y = np.zeros((len(im_list), nldms))
 	im_to_i = {}
+
 	for i in range(nims):
 		im_to_i[ims[i]] = i
-
 	for i in range(len(im_list)):
-		X[i,:] = Xc[im_to_i[im_list[i]],:]
-		Y[i,:] = Yc[im_to_i[im_list[i]],:]
+		X[i, :] = Xc[im_to_i[im_list[i]], :]
+		Y[i, :] = Yc[im_to_i[im_list[i]], :]
 
 	Xc = X
 	Yc = Y
-
-
 	h2 = generate_2_horizontal(options.model_W, options.model_n)
 	v2 = generate_2_vertical(options.model_W, options.model_n)
 	h3 = generate_3_horizontal(options.model_W, options.model_n)
 	v3 = generate_3_vertical(options.model_W, options.model_n)
 	sq = generate_square(options.model_W, options.model_n)
 
-	joblib.dump((h2,v2,h3,v3,sq),'%s%s_lindner_feature_map.pkl'%(model_repo,options.model_name))
+	joblib.dump((h2, v2, h3, v3, sq), '%s%s_lc_feature_map.pkl' % (model_repo, options.model_name))
 	for id_term in term_list:
-		(dataset,rep,img) = build_dataset_image_offset_mp(repository, Xc[:, term_to_i[id_term]], Yc[:, term_to_i[id_term]], im_list, options.model_D_MAX, options.model_n_samples, h2, v2, h3, v3, sq, options.model_njobs)
-		clf = VotingTreeRegressor(n_estimators=options.model_T,n_jobs=options.model_njobs)
-		clf = clf.fit(dataset,rep)
-		joblib.dump(clf, '%s%s_lindner_regressor_%d.pkl' % (model_repo, options.model_name,id_term))
-
-	xt = procrustes(Xc,Yc)
-	(mu,P) = apply_pca(xt,options.model_n_reduc)
-
-	joblib.dump((mu,P),'%s%s_lindner_pca.pkl'%(model_repo,options.model_name))
-
-	F = open('%s%s_lindner_parameters.conf' % (options.model_save_to, options.model_name), 'wb')
+		(dataset, rep, img) = build_dataset_image_offset_mp(repository, Xc[:, term_to_i[id_term]], Yc[:, term_to_i[id_term]], im_list, options.model_D_MAX, options.model_n_samples, h2, v2, h3, v3, sq, options.model_njobs)
+		clf = VotingTreeRegressor(n_estimators=options.model_T, n_jobs=options.model_njobs)
+		clf = clf.fit(dataset, rep)
+		joblib.dump(clf, '%s%s_lc_regressor_%d.pkl' % (model_repo, options.model_name, id_term))
+	xt = procrustes(Xc, Yc)
+	(mu, P) = apply_pca(xt, options.model_n_reduc)
+	joblib.dump((mu, P, np.mean(Xc, axis=0), np.mean(Yc, axis=0)), '%s%s_lc_pca.pkl' % (model_repo, options.model_name))
+	F = open('%s%s_lc_parameters.conf' % (options.model_save_to, options.model_name), 'wb')
 	F.write('cytomine_id_terms %s\n' % options.cytomine_id_terms)
 	F.write('model_njobs %d\n' % options.model_njobs)
 	F.write('model_D_MAX %d\n' % options.model_D_MAX)
@@ -495,3 +503,6 @@ if __name__ == "__main__":
 	F.write('model_R_MIN %d\n' % options.model_R_MIN)
 	F.write('model_alpha %f\n' % options.model_alpha)
 	F.close()
+
+if __name__ == "__main__":
+	main()
