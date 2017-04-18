4734e3abe3e4b49aaf028c8c4a5a9dd91c9a66db
Rémy Vandaele <remy.vandaele@gmail.com>
2017-04-18 17:19:50 +0200

    bii

--- cytomine-applications/landmark_model_builder/build_integral_image.py
+++ cytomine-applications/landmark_model_builder/build_integral_image.py
@@ -1,12 +1,8 @@
 import numpy as np
-import scipy.ndimage as snd
 import scipy.misc as misc
 from multiprocessing import Pool
-import sys,os
 
-"""
-Here we assume the image is one channel only
-"""
+
 def build_integral_image_from_path(path_to_image):
 	img = misc.imread(path_to_image,flatten=True)
 	img = img.astype(float)/255.
@@ -16,7 +12,8 @@ def build_integral_image_from_path(path_to_image):
 		path_to_image = path_to_image.rstrip('.%s'%ext)
 	path_to_image += '_integral'
 	np.save(path_to_image,i_img)
-	
+
+
 def build_integral_image(img):
 	(h,w) = img.shape
 	i_img = np.zeros((h,w))
@@ -31,13 +28,14 @@ def build_integral_image(img):
 			i_img[i,j] = img[i,j]+i_img[i-1,j]+i_img[i,j-1]-i_img[i-1,j-1]
 	return i_img
 
+
 def build_integral_images_mp(tab_path,n_jobs):
 	p = Pool(n_jobs)
 	p.map(build_integral_image_from_path,tab_path)
 	p.close()
 	p.join()
-	
+
+
 if __name__ == "__main__":
-	
 	paths = ['/home/remy/datasets/bigres/%3.3d.bmp'%i for i in range(1,101)]
 	build_integral_images_mp(paths,4)
